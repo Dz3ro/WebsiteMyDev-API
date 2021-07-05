ec87272a1e48215d092937c0e63c999c430206d1
jarek <anderssonjaroslaw@gmail.com>
2021-07-06 00:11:20 +0200

    Another setHeader edit

--- middleware/setHeader.js
+++ middleware/setHeader.js
@@ -10,7 +10,7 @@ function setHeaders(app) {
     );
     res.setHeader(
       "Access-Control-Allow-Headers",
-      "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin"
+      "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin Access-Control-Allow-Methods"
     );
     res.setHeader("Access-Control-Allow-Credentials", true);
     // handle OPTIONS method
